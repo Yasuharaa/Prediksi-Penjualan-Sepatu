323476850aad3a2b93306bea1c947dfd129c6ae6
Yasuharaa <83198193+Yasuharaa@users.noreply.github.com>
2023-11-04 15:50:38 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,9 +4,7 @@ import pandas as pd
 import numpy as np
 from sklearn import linear_model
 from sklearn.model_selection import train_test_split
-import seaborn as sns
 from sklearn import metrics
-import matplotlib.pyplot as plt
 
 model = pickle.load(open('model.sav', 'rb'))
 
