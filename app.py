93c210ea9a7935f2980f5ef8f8b10a7a07f00731
Yasuharaa <83198193+Yasuharaa@users.noreply.github.com>
2023-11-04 18:10:37 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -12,8 +12,10 @@ st.title('Prediksi Penjualan Sepatu berdasarkan Minat Pembeli')
 
 input = st.number_input('Minat')
 minat = np.array([[input]])
-prediksi = ''
+
 
 if st.button('Prediksi Penjualan'):
+    st.write('Jika minat pembeli terhadap suatu merek Sepatu : ', input)
     prediksi = model.predict(minat)
-    st.write('Predikasi Penjualan Sepatu adalah : ', prediksi)
+    st.write('Predikasi Penjualan Sepatu adalah : ')
+    st.success(prediksi)
