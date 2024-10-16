a32137dc3495ef55977b85469ba749cd7ce1130c
Yasuharaa <83198193+Yasuharaa@users.noreply.github.com>
2024-10-16 12:43:50 +0700

    Update app.py

--- app.py
+++ app.py
@@ -15,9 +15,6 @@ st.title('Prediksi Penjualan Sepatu berdasarkan Minat Pembeli di Toko Bambang')
 input_minat = st.number_input('Minat Pembeli', value=0, step=1)
 minat = np.array([[input_minat]])
 
-input_ukuran = st.number_input('Ukuran Sepatu', value=0, step=1)
-ukuran = np.array([[input_minat]])
-
 # Button Prediksi
 if st.button('Prediksi Penjualan'):
     st.write('Jika minat pembeli terhadap suatu merek Sepatu : ', input_minat)
@@ -27,6 +24,6 @@ if st.button('Prediksi Penjualan'):
         prediksi = model.predict(minat)
         prediksi_int = int(prediksi[0])
         st.write('Prediksi Penjualan Sepatu adalah : ')
-        st.success(prediksi_int + ukuran)
+        st.success(prediksi_int)
     except Exception as e:
         st.error(f"An error occurred during prediction: {str(e)}")
